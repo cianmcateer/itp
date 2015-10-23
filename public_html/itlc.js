6f0ea803c43ccfabd68b067431ab47c9a419afa1
cianmcateer <cianmcateer@gmail.com>
2015-10-23 22:04:45 +0100

    todays date

---
+++ public_html/itlc.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+"use strict";
+var randNum = Math.floor((Math.random()*100)+1);
+function guess()
+
+{
+    
+    var guessNum = document.getElementById("input1").value;
+    alert(randNum);
+    if(guessNum == randNum)
+    {
+        
+    }
+    
+    
+}
+
+
